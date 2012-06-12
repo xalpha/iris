665660fd5f7a54d85933fc613e31c6a502aaf1c0
Alex <aduliu@gmail.com>
2012-06-12 11:07:52 +0200

    - updated the licence for each file - added copy of the LGPL v3

--- include/iris/vision.hpp
+++ include/iris/vision.hpp
@@ -1,3 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// This file is part of iris, a lightweight C++ camera calibration library    //
+//                                                                            //
+// Copyright (C) 2010, 2011 Alexandru Duliu                                   //
+//                                                                            //
+// iris is free software; you can redistribute it and/or                      //
+// modify it under the terms of the GNU Lesser General Public                 //
+// License as published by the Free Software Foundation; either               //
+// version 3 of the License, or (at your option) any later version.           //
+//                                                                            //
+// iris is distributed in the hope that it will be useful, but WITHOUT ANY    //
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS  //
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License or the //
+// GNU General Public License for more details.                               //
+//                                                                            //
+// You should have received a copy of the GNU Lesser General Public           //
+// License along with iris. If not, see <http://www.gnu.org/licenses/>.       //
+//                                                                            //
+///////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
 #include <list>

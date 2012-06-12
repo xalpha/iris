aeff9190829c4dc7a0dfd622cd24e268ae7a6128
Alex <aduliu@gmail.com>
2012-06-12 22:20:41 +0200

    updated the license headers of the files

--- src/Finder.cpp
+++ src/Finder.cpp
@@ -2,7 +2,7 @@
 //                                                                            //
 // This file is part of iris, a lightweight C++ camera calibration library    //
 //                                                                            //
-// Copyright (C) 2010, 2011 Alexandru Duliu                                   //
+// Copyright (C) 2012 Alexandru Duliu                                         //
 //                                                                            //
 // iris is free software; you can redistribute it and/or                      //
 // modify it under the terms of the GNU Lesser General Public                 //
